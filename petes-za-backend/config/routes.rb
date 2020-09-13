d37680f909268431606f40ffd8d21d21a0aea270
sean-slaughter <sthompson2822@gmail.com>
2020-09-13 13:52:40 -0500

    Change routes and add OrdersController actions

--- petes-za-backend/config/routes.rb
+++ petes-za-backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :menus, only: [:index]
   resources :items, only: [:show]
-  resources :orders, only: [:create, :show, :edit, :update]
+  resources :orders, except: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

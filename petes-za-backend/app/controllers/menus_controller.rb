787ae5255dd32898e324e854a20625641b3a0206
sean-slaughter <sthompson2822@gmail.com>
2020-09-13 13:51:22 -0500

    Add index action to MenusController

--- petes-za-backend/app/controllers/menus_controller.rb
+++ petes-za-backend/app/controllers/menus_controller.rb
@@ -1,2 +1,6 @@
 class MenusController < ApplicationController
+    def index
+        menus = Menu.all
+        render json: menus, include: [:items, except: [:created_at, :updated_at]]
+    end
 end

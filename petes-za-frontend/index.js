734c3907986421a4d1066fed98f98b28eb718caa
sean-slaughter <sthompson2822@gmail.com>
2020-09-14 12:08:35 -0500

    Added method for rendering Menu

---
+++ petes-za-frontend/index.js
@@ -0,0 +1,6 @@
+//load pizza menu
+const baseURL = "localhost:3000/"
+document.addEventListener("DOMContentLoaded", function(){
+    renderPizzaMenu();
+})
+
--- petes-za-frontend/models/menu.js
+++ petes-za-frontend/models/menu.js
@@ -2,7 +2,19 @@ class Menu{
     constructor(menu){
         this.id = menu["id"];
         this.category = menu["category"]
-        this.items = menu["items"]
-        items.forEach(item => this.items.push(new Item(item)))
+        this.items = []
+        const menuItems = menu["items"]
+        menuItems.forEach(item => this.items.push(new Item(item)))
+    }
+
+    renderItems(){
+        for(item of this.items){
+            const list = document.getElementById("menu-items")
+            const col = document.createElement("div");
+            col.className = "col s6 m3 l4";
+            col.appendChild(item.render());
+            list.appendChild(col);
+        }
+        
     }
 }
